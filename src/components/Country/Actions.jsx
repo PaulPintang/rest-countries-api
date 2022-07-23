8df1e2f81fafcda347252e4204e2f50c54c7ab8a
PaulPintang <pauljpintang@gmail.com>
2022-07-23 23:04:39 +0800

    style: actions spacing  and layout

--- src/components/Country/Actions.jsx
+++ src/components/Country/Actions.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { MdKeyboardArrowDown } from "react-icons/md";
 const Actions = () => {
   return (
-    <div className="flex justify-between items-center w-full h-[110px]">
+    <div className="flex justify-between items-center w-full h-[115px]">
       <div className="relative w-full md:w-auto lg:w-auto">
         <input
-          className="shadow-[0px_0px_5px_0px_#5552524c] px-3 py-2 md:w-56 lg:w-[300px] w-full rounded-md text-sm transition focus:outline-none dark:bg-gray-800 dark:text-gray-100 "
+          className="shadow-[0px_0px_5px_0px_#6564644b] px-3 py-[11px] md:w-56 lg:w-[300px] w-full rounded text-xs transition focus:outline-none dark:bg-gray-800 dark:text-gray-100 "
           type="text"
           placeholder="Select for a country..."
         />
@@ -14,24 +14,13 @@ const Actions = () => {
         <div>
           <button
             type="button"
-            className="flex justify-between md:w-48 lg:w-48 w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none border-opacity-30 dark:bg-gray-800 dark:text-gray-400"
+            className="shadow-[0px_0px_5px_0px_#6564644b] flex justify-between items-center md:w-48 lg:w-36 w-full rounded px-4 py-[11px] bg-white text-sm font-medium focus:outline-none border-opacity-30 dark:bg-gray-800 dark:text-gray-400"
             id="menu-button"
             aria-expanded="true"
             aria-haspopup="true"
           >
-            <svg
-              className="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <small>Filter by region</small>
+            <MdKeyboardArrowDown fontSize={16} />
           </button>
         </div>
       </div>
